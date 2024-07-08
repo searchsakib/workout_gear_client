0167d8c209899a7a605452f555a143144ff39d06
searchsakib <searchsakib@gmail.com>
2024-07-08 23:47:07 +0600

    initial commit

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,12 @@
+import { Button } from '../ui/button';
+
+const MainLayout = () => {
+  return (
+    <div>
+      <h2>This is MainLayout component</h2>
+      <Button>Main Button</Button>
+    </div>
+  );
+};
+
+export default MainLayout;
