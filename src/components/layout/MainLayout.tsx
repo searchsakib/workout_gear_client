6a37ffb76f47f312778314815bc68f5675fa75ed
searchsakib <searchsakib@gmail.com>
2024-07-11 04:02:33 +0600

    some basic stuff

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -2,7 +2,7 @@ import { Button } from '../ui/button';
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className='bg-primary-gradient h-screen'>
       <h2>This is MainLayout component</h2>
       <Button>Main Button</Button>
     </div>
@@ -10,3 +10,5 @@ const MainLayout = () => {
 };
 
 export default MainLayout;
+
+// bg-[#07001f]
---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+  return (
+    <div>
+      <h2>This is ErrorPage component</h2>
+    </div>
+  );
+};
+
+export default ErrorPage;
