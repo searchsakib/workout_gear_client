6eb11220a1694d153191f24ce5faac1955f08176
searchsakib <searchsakib@gmail.com>
2024-07-12 12:43:15 +0600

    product management functionality done

--- src/pages/ProductManagement/ProductManagement.tsx
+++ src/pages/ProductManagement/ProductManagement.tsx
@@ -9,6 +9,7 @@ import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { useState } from "react";
 import ProductForm from "./ProductForm";
 import ProductTable from "./ProductTable";
+import Swal from "sweetalert2";
 
 const ProductManagement: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -21,20 +22,76 @@ const ProductManagement: React.FC = () => {
   );
   const [isAdding, setIsAdding] = useState(false);
 
-  const handleAddProduct = async (product: any) => {
-    await addProduct(product);
-    setIsAdding(false);
+  const handleAddProduct = (product: any) => {
+    addProduct(product).then((res) => {
+      setIsAdding(false);
+      console.log(res.data);
+      if (res?.data?.success) {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          color: "#fff",
+          iconColor: "#fff",
+          background: "#09335c",
+          confirmButtonColor: "#3085d6",
+          title: `Product added Successfully!`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
   };
 
-  const handleUpdateProduct = async (product: any) => {
-    await updateProduct({ id: selectedProduct?._id, ...product });
-    dispatch(setSelectedProduct(null));
+  const handleUpdateProduct = (product: any) => {
+    updateProduct({ id: selectedProduct?._id, ...product }).then((res) => {
+      dispatch(setSelectedProduct(null));
+      console.log(res.data);
+      if (res?.data?.success) {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          color: "#fff",
+          iconColor: "#fff",
+          background: "#09335c",
+          confirmButtonColor: "#3085d6",
+          title: `Product Updated Successfully!`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
   };
 
-  const handleDeleteProduct = async (id: string) => {
-    if (window.confirm("Are you sure you want to delete this product?")) {
-      await deleteProduct(id);
-    }
+  const handleDeleteProduct = (id: string) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      color: "#fff",
+      iconColor: "#fff",
+      background: "#09335c",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteProduct(id).then((res: any) => {
+          console.log(res);
+          if (res?.data?.success) {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your Product has been deleted.",
+              icon: "success",
+              color: "#fff",
+              iconColor: "#fff",
+              background: "#09335c",
+              confirmButtonColor: "#3085d6",
+            });
+          }
+        });
+      }
+    });
   };
 
   return (
