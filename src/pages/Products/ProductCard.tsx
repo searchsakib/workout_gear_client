134aebbd8cd4778dcc4e1f3e216e86a6a1e5271d
searchsakib <searchsakib@gmail.com>
2024-07-13 05:30:33 +0600

    used flex-1 to push button to bottom, design changed

--- src/pages/Products/ProductCard.tsx
+++ src/pages/Products/ProductCard.tsx
@@ -6,17 +6,17 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   return (
-    <div className="rounded-md bg-white p-4 shadow-md">
+    <div className="card rounded-md bg-transparent p-4 shadow-md outline outline-1">
       <img
         src={product?.images}
         alt={product?.name}
         className="mb-4 h-40 w-full rounded-md object-cover"
       />
       <div className="mb-2 flex items-center justify-between">
-        <h2 className="text-lg font-semibold text-gray-800">{product?.name}</h2>
-        <span className="text-gray-600">${product?.price.toFixed(2)}</span>
+        <h2 className="text-lg font-semibold text-gray-100">{product?.name}</h2>
+        <span className="text-gray-300">${product?.price.toFixed(2)}</span>
       </div>
-      <p className="text-gray-600">{product?.description}</p>
+      <p className="flex-1 text-gray-300">{product?.description}</p>
       <div className="mt-4 flex justify-end">
         <button className="rounded-md bg-blue-500 px-4 py-2 text-white">
           View Details
